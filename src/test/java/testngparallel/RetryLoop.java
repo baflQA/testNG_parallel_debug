3cd64c51841ed77262a0793c04ab8eed35c0d954
Bartek Florczak <bflorczak@outlook.com>
2019-10-02 16:05:24 +0200

    Add positive scenario

--- src/test/java/testngparallel/RetryLoop.java
+++ src/test/java/testngparallel/RetryLoop.java
@@ -14,16 +14,29 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class RetryLoop {
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private AtomicInteger atomicInt = new AtomicInteger(0);
+	Object existingObject = new Object();
 
 	@RetriesCount(3)
-	@Test(retryAnalyzer = RetryAnalyzer.class, dataProvider = "dp3")
-	public void test1(Object object) {
+	@Test(retryAnalyzer = RetryAnalyzer.class, dataProvider = "dpNewObject")
+	public void willNotStopAfter3Failures(Object newObject) {
+		logger.info("Run: " + atomicInt.incrementAndGet());
+		Assert.fail("Kaboom!");
+	}
+
+	@RetriesCount(3)
+	@Test(retryAnalyzer = RetryAnalyzer.class, dataProvider = "dpNewSameObject")
+	public void willStopAfter3Failures(Object existingObject) {
 		logger.info("Run: " + atomicInt.incrementAndGet());
 		Assert.fail("Kaboom!");
 	}
 
 	@DataProvider
-	public Object[][] dp3() {
+	public Object[][] dpNewObject() {
 		return new Object[][]{new Object[] {new Object()}};
+	}
+
+	@DataProvider
+	public Object[][] dpNewSameObject() {
+		return new Object[][]{new Object[] {existingObject}};
 	}
 }

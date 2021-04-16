765df1fd47635624f297f6ed8476c5d2c8ea9642
Bartlomiej Florczak <b.florczak@sap.com>
2021-04-16 12:21:20 +0200

    Add failing before method

--- src/main/java/testngparallel/jarexitcode/Fail.java
+++ src/main/java/testngparallel/jarexitcode/Fail.java
@@ -1,9 +1,15 @@
 package testngparallel.jarexitcode;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Fail {
+
+    @BeforeMethod
+    public void failedConf() {
+        Assert.fail();
+    }
 
     @Test
     public void failedTest() {

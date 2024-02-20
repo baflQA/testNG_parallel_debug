23ae69a335eebb085948f7d772b05d6028bf0f03
Bartek Florczak <zaworro@gmail.com>
2024-02-20 17:52:43 +0100

    Update SkipFirstBeforeMethodExecution.java

--- src/test/java/testngparallel/SkipFirstBeforeMethodExecution.java
+++ src/test/java/testngparallel/SkipFirstBeforeMethodExecution.java
@@ -5,14 +5,21 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.testng.Assert.assertEquals;
 
 public class SkipFirstBeforeMethodExecution {
 
+    AtomicInteger atomicInteger = new AtomicInteger();
+
     @BeforeClass
     public void createWebdriverAndNavigate() {
+        //e.g.
         //webdriver = new WebDriver
         //webdriver.logIn()
+
+        System.out.println("Before class");
     }
 
 
@@ -20,18 +27,27 @@ public class SkipFirstBeforeMethodExecution {
             //skipFirstExecution = true
     )
     public void refreshPageBefore() {
+        //e.g.
         //webdriver.refresh()
+        System.out.println("Before method");
+        atomicInteger.incrementAndGet();
     }
 
-    @Test
-    public void test() {
-        assertTrue(true);
+    @Test(priority = 1)
+    public void test1() {
+        assertEquals(atomicInteger.get(), 0); //before method skipped for the first test
+    }
+
+    @Test(priority = 2)
+    public void test2() {
+        assertEquals(atomicInteger.get(), 1); //before method executed for the second test
     }
 
     @AfterMethod(
             //skipLastExecution = true
     )
     public void refreshPageAfterMethod() {
+        //e.g.
         //webdriver.refresh()
     }
 }
